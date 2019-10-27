3dc16476fb22cf0a35aedad06a1cfc4830cfa211
TeamFishbowl <57051466+TeamFishbowl@users.noreply.github.com>
2019-10-27 08:57:55 +0800

    Create main.lua

---
+++ server/main.lua
@@ -0,0 +1,166 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+
+ESX.RegisterUsableItem('extendedclip', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)	
+    TriggerClientEvent('esx_attachments_bleiker:extendedclip', source)
+
+end)
+
+ESX.RegisterUsableItem('compensator', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)	
+    TriggerClientEvent('esx_attachments_bleiker:compensator', source)
+
+end)
+
+ESX.RegisterUsableItem('pistolscope', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)	
+    TriggerClientEvent('esx_attachments_bleiker:pistolscope', source)
+
+end)
+
+ESX.RegisterUsableItem('goldskin', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)	
+    TriggerClientEvent('esx_attachments_bleiker:goldskin', source)
+
+end)
+
+ESX.RegisterUsableItem('riflescope', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)	
+    TriggerClientEvent('esx_attachments_bleiker:riflescope', source)
+
+end)
+
+ESX.RegisterUsableItem('butterflyskin', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)	
+    TriggerClientEvent('esx_attachments_bleiker:butterflyskin', source)
+
+end)
+
+ESX.RegisterUsableItem('loveknuckleskin', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)	
+    TriggerClientEvent('esx_attachments_bleiker:loveknuckleskin', source)
+
+end)
+
+ESX.RegisterUsableItem('mediumscope', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)	
+    TriggerClientEvent('esx_attachments_bleiker:mediumscope', source)
+
+end)
+
+ESX.RegisterUsableItem('largescope', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)	
+    TriggerClientEvent('esx_attachments_bleiker:largescope', source)
+
+end)
+
+ESX.RegisterUsableItem('holosight', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)	
+    TriggerClientEvent('esx_attachments_bleiker:holosight', source)
+
+end)
+
+ESX.RegisterUsableItem('platinumpistolskin', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)	
+    TriggerClientEvent('esx_attachments_bleiker:platinumpistolskin', source)
+
+end)
+
+ESX.RegisterUsableItem('woodheavypistolskin', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)	
+    TriggerClientEvent('esx_attachments_bleiker:woodheavypistolskin', source)
+
+end)
+
+ESX.RegisterUsableItem('woodrifleskin', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)	
+    TriggerClientEvent('esx_attachments_bleiker:woodrifleskin', source)
+
+end)
+
+ESX.RegisterUsableItem('skullrifleskin', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)	
+    TriggerClientEvent('esx_attachments_bleiker:skullrifleskin', source)
+
+end)
+
+ESX.RegisterUsableItem('zebrarifleskin', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)	
+    TriggerClientEvent('esx_attachments_bleiker:zebrarifleskin', source)
+
+end)
+
+ESX.RegisterUsableItem('boomrifleskin', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)	
+    TriggerClientEvent('esx_attachments_bleiker:boomrifleskin', source)
+
+end)
+
+ESX.RegisterUsableItem('muzzlebrake', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)	
+    TriggerClientEvent('esx_attachments_bleiker:muzzlebrake', source)
+
+end)
+
+ESX.RegisterUsableItem('pimpknuckles', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)	
+    TriggerClientEvent('esx_attachments_bleiker:pimpknuckles', source)
+
+end)
+
+ESX.RegisterUsableItem('ballasknuckle', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)	
+    TriggerClientEvent('esx_attachments_bleiker:ballasknuckle', source)
+
+end)
+
+ESX.RegisterUsableItem('dollarknuckle', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)	
+    TriggerClientEvent('esx_attachments_bleiker:dollarknuckle', source)
+
+end)
+
+ESX.RegisterUsableItem('diamondknuckle', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)	
+    TriggerClientEvent('esx_attachments_bleiker:diamondknuckle', source)
+
+end)
+
+ESX.RegisterUsableItem('hateknuckles', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)	
+    TriggerClientEvent('esx_attachments_bleiker:hateknuckles', source)
+
+end)
+
+ESX.RegisterUsableItem('playerknuckles', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)	
+    TriggerClientEvent('esx_attachments_bleiker:playerknuckles', source)
+
+end)
+
+ESX.RegisterUsableItem('vagosknuckles', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)	
+    TriggerClientEvent('esx_attachments_bleiker:vagosknuckles', source)
+
+end)
+
+ESX.RegisterUsableItem('grip', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)	
+    TriggerClientEvent('esx_attachments_bleiker:grip', source)
+
+end)
+
+ESX.RegisterUsableItem('Suppressor', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)	
+    TriggerClientEvent('esx_attachments_bleiker:Suppressor', source)
+
+end)
+
+ESX.RegisterUsableItem('flashlight', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)	
+    TriggerClientEvent('esx_attachments_bleiker:flashlight', source)
+
+end)
